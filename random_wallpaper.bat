cb8078aea9a1b6029a86f0e00277eee5193acb9a
Valmiki Kothare <valmiki.kothare.vk@gmail.com>
2022-12-21 21:14:07 -0500

    Minor fixes

---
+++ random_wallpaper.bat
@@ -0,0 +1,2 @@
+pushd "%~dp0"
+"C:\Users\valmi\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\valmi\Documents\Studio Ghibli Wallpaper\random_wallpaper.py"
--- set_wallpaper.bat
+++
@@ -1,2 +0,0 @@
-cd %~dp0
-"C:\Users\valmi\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\valmi\Documents\Studio Ghibli Wallpaper\set_wallpaper.py"
